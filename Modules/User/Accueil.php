65aa923a72f4d08e8aedfb4a5e945507ab4f14c1
Thomas <th.lebret@outlook.fr>
2020-04-27 21:58:36 +0200

    Refactoring

--- Modules/User/Accueil.php
+++ Modules/User/Accueil.php
@@ -1,44 +1,44 @@
 <!doctype html>
 <html lang="fr">
-	<head>
-		<meta charset="utf-8" />
-		<title> Accueil </title>
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-		<link rel="stylesheet" type="text/css" href="style/style.css" />
-		<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-	</head>
+<head>
+	<meta charset="utf-8" />
+	<title> Accueil </title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="style/style.css" />
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</head>
 
-	<?php include('index.php'); ?>
-	
-	<body>
-			
-		<div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
-		  <ol class="carousel-indicators">
+<?php include('index.php'); ?>
+
+<body>
+
+	<div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+		<ol class="carousel-indicators">
 			<li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
 			<li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
 			<li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-		  </ol>
-		  <div class="carousel-inner">
+		</ol>
+		<div class="carousel-inner">
 			<div class="carousel-item active">
-			  <img src="img/Maintenance2.jpg" class="d-block w-100" alt="...">
+				<img src="img/Maintenance2.jpg" class="d-block w-100" alt="...">
 			</div>
 			<div class="carousel-item">
-			  <img src="img/Ferret.jpg" class="d-block w-100" alt="...">
+				<img src="img/Ferret.jpg" class="d-block w-100" alt="...">
 			</div>
 			<div class="carousel-item">
-			  <img src="img/Spectacle.jpg" class="d-block w-100" alt="...">
+				<img src="img/Spectacle.jpg" class="d-block w-100" alt="...">
 			</div>
-		  </div>
-		  <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+		</div>
+		<a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
 			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
 			<span class="sr-only">Previous</span>
-		  </a>
-		  <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+		</a>
+		<a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
 			<span class="carousel-control-next-icon" aria-hidden="true"></span>
 			<span class="sr-only">Next</span>
-		  </a>
-		</div>
-	</body>
+		</a>
+	</div>
+</body>
 </html>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+require_once("constantes.php");
+require_once(CHEMIN_GLOBAL."head.html");
+require_once(CHEMIN_GLOBAL."menu.html");
+
+
+require_once(CHEMIN_GLOBAL."bottom.html");
