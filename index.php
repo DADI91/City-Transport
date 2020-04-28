42b44b13c5c50b314b98debe0cdb93e899ec42a3
Thomas <th.lebret@outlook.fr>
2020-04-28 17:15:20 +0200

    Mise en place des controlleurs Site en une page en cours

--- index.php
+++ index.php
@@ -1,5 +1,31 @@
 <?php
 require_once("Global/head.html");
 require_once("Global/menu.html");
+
+if(!empty($_GET['page'])) {
+	switch($_GET['page']) {
+		case 'accueil':
+			require_once("Controllers/accueil.php");
+			break;
+		case 'connexion':
+			require_once("Controllers/accueil.php");
+			break;
+		case 'etat_trafic':
+			require_once("Controllers/etat_trafic.php");
+			break;
+		case 'inscription':
+			require_once("Controllers/inscription.php");
+			break;
+		case 'itineraire':
+			require_once("Controllers/itineraire.php");
+			break;
+		default:
+			header("Location: index.php?page=accueil");
+			break;
+	}
+} else {
+	header("Location: index.php?page=accueil");
+}
+
 require_once("Global/bottom.html");
 ?>
